279f5ff2b2d14d69a5da5e56f06118440cf3bcf5
Neve Laughery <RewoundVHS@protonmail.com>
2026-05-21 15:03:47 -0500

    Handle double faced cards

--- momir.py
+++ momir.py
@@ -6,22 +6,34 @@ from json import loads
 # Query Scryfall for a random creature of the specified mana value
 # Limit these creatures to only those that are legal in Vintage to avoid 
 # stickers, attractions and culturally insensitive cards
-# This query also excludes double-faced creatures
 def fetch_card_data(mana_value):
-    card = loads(get(f"https://api.scryfall.com/cards/random?q=t%3Acreature%20-is%3A:double-faced%20legal%3Avintage%20mv%3A{mana_value}").text)
+	card = loads(get(fr"https://api.scryfall.com/cards/random?q=t%3A%2F^[^\%2F]*Creature%2F%20legal%3Avintage%20mv%3A{mana_value}").text)
 
-    remove_braces = str.maketrans('', '', '{}')
+	remove_braces = str.maketrans('', '', '{}')
 
-    if card['object'] == 'error':
-        return 'Error'
-    else:
-        # Compile the list of relevant card data
-        card_data = {
-            'name': card['name'], 
-            'mana_cost': card['mana_cost'].translate(remove_braces), 
-            'type_line': card['type_line'], 
-            'oracle_text': card['oracle_text'], 
-            'power_toughness': card['power'] + '/' + card['toughness'], 
-            'art': card['image_uris']['art_crop']
-        }
-    return card_data
+	if card['object'] == 'error':
+		return 'Error'
+    # Handle double faced cards
+	elif 'card_faces' in card:
+		card_data = {
+			'name': card['card_faces'][0]['name'], 
+			'mana_cost': card['card_faces'][0]['mana_cost'].translate(remove_braces), 
+			'type_line': card['card_faces'][0]['type_line'], 
+			'oracle_text': card['card_faces'][0]['oracle_text'], 
+			'power_toughness': card['card_faces'][0]['power'] + '/' 
+				+ card['card_faces'][0]['toughness'] 
+				if 'Creature' in card['card_faces'][0]['type_line'] else '', 
+			'art': card['card_faces'][0]['image_uris']['art_crop'], 
+		}
+	else:
+		# Compile the list of relevant card data
+		card_data = {
+			'name': card['name'], 
+			'mana_cost': card['mana_cost'].translate(remove_braces), 
+			'type_line': card['type_line'], 
+			'oracle_text': card['oracle_text'], 
+			'power_toughness': card['power'] + '/' + card['toughness'] 
+				if 'Creature' in card['type_line'] else '', 
+			'art': card['image_uris']['art_crop']
+		}
+	return card_data
