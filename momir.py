a0a137ae6fcb98e83ee5da9c2e9afa038def8ed1
Neve Laughery <RewoundVHS@protonmail.com>
2026-05-27 13:41:26 -0500

    Started refactoring

--- momir.py
+++ momir.py
@@ -3,37 +3,45 @@
 from requests import get
 from json import loads
 
+# Compile the list of relevant card data
+def fetch_card_data(card):
+    remove_braces = str.maketrans('', '', '{}')
+
+    if card['object'] == 'error':
+        return 'Error'
+    # Find data for a double faced card
+    elif 'card_faces' in card:
+        card_data = {
+            'name': card['card_faces'][0]['name'], 
+            'mana_cost': card['card_faces'][0]['mana_cost'].translate(remove_braces), 
+            'type_line': card['card_faces'][0]['type_line'], 
+            'oracle_text': card['card_faces'][0]['oracle_text'], 
+            'power_toughness': card['card_faces'][0]['power'] + '/' 
+                + card['card_faces'][0]['toughness'] 
+                if 'Creature' in card['card_faces'][0]['type_line'] else '', 
+            'art': card['card_faces'][0]['image_uris']['art_crop'], 
+        }
+    else:
+        # Find data for a single faced card
+        card_data = {
+            'name': card['name'], 
+            'mana_cost': card['mana_cost'].translate(remove_braces), 
+            'type_line': card['type_line'], 
+            'oracle_text': card['oracle_text'], 
+            'power_toughness': card['power'] + '/' + card['toughness'] 
+                if 'Creature' in card['type_line'] else '', 
+            'art': card['image_uris']['art_crop']
+        }
+    return card_data
+
 # Query Scryfall for a random creature of the specified mana value
 # Limit these creatures to only those that are legal in Vintage to avoid 
 # stickers, attractions and culturally insensitive cards
-def fetch_card_data(mana_value):
-	card = loads(get(fr"https://api.scryfall.com/cards/random?q=t%3A%2F^[^\%2F]*Creature%2F%20legal%3Avintage%20mv%3A{mana_value}").text)
+def random_card_by_mana_value(mana_value):
+    card = loads(get(fr"https://api.scryfall.com/cards/random?q=t%3A%2F^[^\%2F]*Creature%2F%20legal%3Avintage%20mv%3A{mana_value}").text)
+    return card
 
-	remove_braces = str.maketrans('', '', '{}')
-
-	if card['object'] == 'error':
-		return 'Error'
-    # Handle double faced cards
-	elif 'card_faces' in card:
-		card_data = {
-			'name': card['card_faces'][0]['name'], 
-			'mana_cost': card['card_faces'][0]['mana_cost'].translate(remove_braces), 
-			'type_line': card['card_faces'][0]['type_line'], 
-			'oracle_text': card['card_faces'][0]['oracle_text'], 
-			'power_toughness': card['card_faces'][0]['power'] + '/' 
-				+ card['card_faces'][0]['toughness'] 
-				if 'Creature' in card['card_faces'][0]['type_line'] else '', 
-			'art': card['card_faces'][0]['image_uris']['art_crop'], 
-		}
-	else:
-		# Compile the list of relevant card data
-		card_data = {
-			'name': card['name'], 
-			'mana_cost': card['mana_cost'].translate(remove_braces), 
-			'type_line': card['type_line'], 
-			'oracle_text': card['oracle_text'], 
-			'power_toughness': card['power'] + '/' + card['toughness'] 
-				if 'Creature' in card['type_line'] else '', 
-			'art': card['image_uris']['art_crop']
-		}
-	return card_data
+# Query Scryfall for a card with the specified card name
+def search_card_by_name(card_name):
+    card = loads(get(fr"https://api.scryfall.com/cards/named?fuzzy={card_name}").text)
+    return card
--- render_card.py
+++ render_card.py
@@ -16,153 +16,155 @@ FONT_NAME = "Ubuntu-Sans-Regular"
 FONT_TYPE = "Ubuntu-Sans-Regular"
 FONT_RULES = "Ubuntu-Sans-Regular"
  
-foundCard = momir.fetch_card_data(input("Mana Value: "))
+random_card = momir.random_card_by_mana_value(input("Mana Value: "))
+found_card = momir.fetch_card_data(random_card)
  
-if foundCard == 'Error':
-	print('Error, card not found')
+if found_card == 'Error':
+    print('Error, card not found')
+    quit()
 else:
-	# Print the card data
-	print("Name:", foundCard['name'])
-	print("Mana Cost:", foundCard['mana_cost'])
-	print("Type Line:", foundCard['type_line'])
-	print("Oracle Text:", foundCard['oracle_text'])
-	print("Power/Toughness:", foundCard['power_toughness'])
-	print("Art URL:", foundCard['art'])
+    # Print the card data
+    print("Name:", found_card['name'])
+    print("Mana Cost:", found_card['mana_cost'])
+    print("Type Line:", found_card['type_line'])
+    print("Oracle Text:", found_card['oracle_text'])
+    print("Power/Toughness:", found_card['power_toughness'])
+    print("Art URL:", found_card['art'])
  
-	print("Downloading art...")
-	response = get(foundCard['art'])
-	with open('art.png', 'wb') as f:
-		f.write(response.content)
+    print("Downloading art...")
+    response = get(found_card['art'])
+    with open('art.png', 'wb') as f:
+        f.write(response.content)
  
 def run(cmd):
-	return subprocess.check_output(cmd).decode().strip()
+    return subprocess.check_output(cmd).decode().strip()
 
 # Attempt to fit the text within the specified width
 def fit_text_width(text, font, max_size, min_size, max_width):
-	for size in range(max_size, min_size - 1, -1):
-		w = run([
-			"convert",
-			"-font", font,
-			"-pointsize", str(size),
-			f"label:{text}",
-			"-format", "%w",
-			"info:"
-		])
-		if int(w) <= max_width:
-			return size
-	return min_size
+    for size in range(max_size, min_size - 1, -1):
+        w = run([
+            "convert",
+            "-font", font,
+            "-pointsize", str(size),
+            f"label:{text}",
+            "-format", "%w",
+            "info:"
+        ])
+        if int(w) <= max_width:
+            return size
+    return min_size
  
 # Attempt to fit the text within the specified height
 def fit_text_height(text, font, max_size, min_size, width, height):
-	for size in range(max_size, min_size - 1, -1):
-		h = run([
-			"convert",
-			"-size", f"{width}x",
-			"-font", font,
-			"-pointsize", str(size),
-			f"caption:{text}",
-			"-format", "%h",
-			"info:"
-		])
-		if int(h) <= height:
-			return size
-	return min_size
+    for size in range(max_size, min_size - 1, -1):
+        h = run([
+            "convert",
+            "-size", f"{width}x",
+            "-font", font,
+            "-pointsize", str(size),
+            f"caption:{text}",
+            "-format", "%h",
+            "info:"
+        ])
+        if int(h) <= height:
+            return size
+    return min_size
  
 def main():
-	# Fit text sizes 
-	name_size = fit_text_width(foundCard["name"], FONT_NAME, 48, 28, NAME_MAX_WIDTH)
-	type_size = fit_text_width(foundCard["type_line"], FONT_TYPE, 36, 20, TEXT_WIDTH)
-	oracle_size = fit_text_height(foundCard["oracle_text"], FONT_RULES, 30, 12, TEXT_WIDTH, ORACLE_HEIGHT)
+    # Fit text sizes 
+    name_size = fit_text_width(found_card["name"], FONT_NAME, 48, 28, NAME_MAX_WIDTH)
+    type_size = fit_text_width(found_card["type_line"], FONT_TYPE, 36, 20, TEXT_WIDTH)
+    oracle_size = fit_text_height(found_card["oracle_text"], FONT_RULES, 30, 12, TEXT_WIDTH, ORACLE_HEIGHT)
  
-	# Create base 
-	subprocess.run([
-		"convert",
-		"-size", f"{CARD_WIDTH}x{CARD_HEIGHT}",
-		"xc:white",
-		"card.png"
-	], check=True)
+    # Create base 
+    subprocess.run([
+        "convert",
+        "-size", f"{CARD_WIDTH}x{CARD_HEIGHT}",
+        "xc:white",
+        "card.png"
+    ], check=True)
 
-	# Add name 
-	subprocess.run([
-		"convert", "card.png",
-		"-font", FONT_NAME,
-		"-pointsize", str(name_size),
-		"-gravity", "NorthWest",
-		"-annotate", "+60+10", foundCard["name"],
-		"card.png"
-	], check=True)
+    # Add name 
+    subprocess.run([
+        "convert", "card.png",
+        "-font", FONT_NAME,
+        "-pointsize", str(name_size),
+        "-gravity", "NorthWest",
+        "-annotate", "+60+10", found_card["name"],
+        "card.png"
+    ], check=True)
 
-	# Add mana cost
-	subprocess.run([
-		"convert", "card.png",
-		"-font", FONT_NAME,
-		"-pointsize", "36",
-		"-gravity", "NorthEast",
-		"-annotate", "+60+10", foundCard["mana_cost"],
-		"card.png"
-	], check=True)
+    # Add mana cost
+    subprocess.run([
+        "convert", "card.png",
+        "-font", FONT_NAME,
+        "-pointsize", "36",
+        "-gravity", "NorthEast",
+        "-annotate", "+60+10", found_card["mana_cost"],
+        "card.png"
+    ], check=True)
  
-	# Crop art 
-	subprocess.run([
-		"convert", "art.png",
-		"-resize", f"{ART_WIDTH}x{ART_HEIGHT}^",
-		"-gravity", "center",
-		"-extent", f"{ART_WIDTH}x{ART_HEIGHT}",
-		"art_cropped.png"
-	], check=True)
+    # Crop art 
+    subprocess.run([
+        "convert", "art.png",
+        "-resize", f"{ART_WIDTH}x{ART_HEIGHT}^",
+        "-gravity", "center",
+        "-extent", f"{ART_WIDTH}x{ART_HEIGHT}",
+        "art_cropped.png"
+    ], check=True)
 
-	# Add art 
-	subprocess.run([
-		"convert", "card.png",
-		"art_cropped.png",
-		"-geometry", "+60+80",
-		"-composite",
-		"card.png"
-	], check=True)
+    # Add art 
+    subprocess.run([
+        "convert", "card.png",
+        "art_cropped.png",
+        "-geometry", "+60+80",
+        "-composite",
+        "card.png"
+    ], check=True)
  
-	# Add type line 
-	subprocess.run([
-		"convert", "card.png",
-		"-font", FONT_TYPE,
-		"-pointsize", str(type_size),
-		"-gravity", "NorthWest",
-		"-annotate", "+60+560", foundCard["type_line"],
-		"card.png"
-	], check=True)
+    # Add type line 
+    subprocess.run([
+        "convert", "card.png",
+        "-font", FONT_TYPE,
+        "-pointsize", str(type_size),
+        "-gravity", "NorthWest",
+        "-annotate", "+60+560", found_card["type_line"],
+        "card.png"
+    ], check=True)
 
-	# Render oracle text
-	subprocess.run([
-		"convert",
-		"-size", f"{TEXT_WIDTH}x",
-		"-font", FONT_RULES,
-		"-pointsize", str(oracle_size),
-		"-background", "white",
-		f"caption:{foundCard['oracle_text']}",
-		"-gravity", "NorthWest",
-		"-extent", f"{TEXT_WIDTH}x{ORACLE_HEIGHT}",
-		"oracle.png"
-	], check=True)
+    # Render oracle text
+    subprocess.run([
+        "convert",
+        "-size", f"{TEXT_WIDTH}x",
+        "-font", FONT_RULES,
+        "-pointsize", str(oracle_size),
+        "-background", "white",
+        f"caption:{found_card['oracle_text']}",
+        "-gravity", "NorthWest",
+        "-extent", f"{TEXT_WIDTH}x{ORACLE_HEIGHT}",
+        "oracle.png"
+    ], check=True)
 
-	# Add oracle text 
-	subprocess.run([
-		"convert", "card.png",
-		"oracle.png",
-		"-geometry", "+60+610",
-		"-composite",
-		"card.png"
-	], check=True)
+    # Add oracle text 
+    subprocess.run([
+        "convert", "card.png",
+        "oracle.png",
+        "-geometry", "+60+610",
+        "-composite",
+        "card.png"
+    ], check=True)
  
-	# Add power/toughness 
-	subprocess.run([
-		"convert", "card.png",
-		"-font", FONT_NAME,
-		"-pointsize", "36",
-		"-gravity", "SouthEast",
-		"-annotate", "+60+80", foundCard["power_toughness"],
-		"output.png"
-	], check=True)
+    # Add power/toughness 
+    subprocess.run([
+        "convert", "card.png",
+        "-font", FONT_NAME,
+        "-pointsize", "36",
+        "-gravity", "SouthEast",
+        "-annotate", "+60+80", found_card["power_toughness"],
+        "output.png"
+    ], check=True)
  
-	print("Generated: output.png")
+    print("Generated: output.png")
  
 if __name__ == "__main__":
-	main()
+    main()
